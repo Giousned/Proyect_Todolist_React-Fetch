19916c780b8ab344d4107e3ba907a5994ab65691
Giousned <guille_ranza@hotmail.com>
2023-03-13 12:42:17 +0000

    [ADD] funciones externar y exportadas para los fetch

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -3,78 +3,37 @@ import { useState, useEffect } from "react";
 
 import ElementList from "./ElementList.jsx";
 
+import {handlePOSTUserCreate, handleGET_TodoList, handlePUT_NewTodo, handlePUT_DeleteTodo} from "../store/config.js"
+
 import "../../styles/todoList.css";
 
 
-const TodoList = () => {
+const TodoList = (props) => {
   const [value, setValue] = useState("");
   const [estado, setEstado] = useState([]);
 
   // ESTA FUNCION SOLO HAY QUE EJECUTARLA 1 VEZ, LA PRIMERA VEZ PARA CREAR EL USUARIO
   // POR ESO LO HACEMOS CON USEEFFECT
+
   function crearUsuario () {
-    fetch('https://assets.breatheco.de/apis/fake/todos/user/guilleJFG', {
-      method: "POST",
-      body: JSON.stringify(estado),
-      headers: {
-        "Content-Type": "application/json",
-      }
-    })
-    .then((resp) => {
-      console.log(resp.ok); // will be true if the response is successfull
-      return resp; //(returns promise) will try to parse the result as json as return a promise that you can .then for results
-    })
-    .then((data) => {
-      //here is were your code should start after the fetch finishes
-      getUSers();
-    })
-    .catch(error => {console.log(error);});  //Error handling
+    handlePOSTUserCreate(estado,getUSers);
   }
 
   // CUANDO EL USUARIO CON ESE NOMBRE YA ESTÉ CREADO, YA NO HACE FALTA USARLA MAS VECES
-  useEffect(() => {crearUsuario()},[]);
+  // useEffect(() => {crearUsuario()},[]);
 
   function getUSers () {
-    fetch('https://assets.breatheco.de/apis/fake/todos/user/guilleJFG', {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    .then(resp => {
-        console.log(resp.ok); // will be true if the response is successfull
-        return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
-    })
-    .then(data => {
-        setEstado(data);
-    })
-    .catch(error => {console.log(error);});  //Error handling
+    handleGET_TodoList(setEstado);    
   }
 
-  // useEffect(() => {getUSers()},[]);
+  useEffect(() => {getUSers()},[]);
 
   function addTasks (value) {
 
     const newObj = {label: value, done: false};
     const newArr = [...estado,newObj];
 
-    fetch('https://assets.breatheco.de/apis/fake/todos/user/guilleJFG', {
-        method: "PUT",
-        body: JSON.stringify(newArr),
-        headers: {
-          "Content-Type": "application/json"
-      }
-      })
-      .then(resp => {
-          console.log(resp.status); // the status code = 200 or code = 400 etc.
-          return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
-      })
-      .then(data => {
-          //here is were your code should start after the fetch finishes
-          console.log(data); //this will print on the console the exact object received from the server
-          getUSers();
-      })
-      .catch(error => {console.log(error);});  //Error handling
+    handlePUT_NewTodo(newArr,getUSers);
   }
 
 
@@ -94,22 +53,7 @@ const TodoList = () => {
     const newArr2 = estado.filter((item) => item != elemento);
     setEstado(newArr2);
 
-    fetch('https://assets.breatheco.de/apis/fake/todos/user/guilleJFG', {
-        method: "PUT",
-        body: JSON.stringify(newArr2),
-        headers: {
-          "Content-Type": "application/json"
-        }
-    })
-    .then(resp => {
-        console.log(resp.status); // the status code = 200 or code = 400 etc.
-        return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
-    })
-    .then(data => {
-        //here is were your code should start after the fetch finishes
-        console.log(data); //this will print on the console the exact object received from the server
-    })
-    .catch(error => {console.log(error);});  //Error handling
+    handlePUT_DeleteTodo(newArr2,getUSers);
   }
 
 
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,28 +3,15 @@ import React from "react";
 import TodoList from "./TodoList.jsx";
 import Boton from "./Boton.jsx";
 
+import {handleDELETE_TodosAndUser} from "../store/config.js"
+
 //create your first component
 const Home = () => {
 
 	const handleDeleteAll = () => {
-
-		fetch('https://assets.breatheco.de/apis/fake/todos/user/guilleJFG', {
-			method: "DELETE",
-			headers: {
-				"Content-Type": "application/json"
-			}
-		})
-		.then(resp => {
-			console.log(resp.ok); // will be true if the response is successfull
-			console.log(resp.status); // the status code = 200 or code = 400 etc.
-			return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
-		})
-		.then(data => {
-			//here is were your code should start after the fetch finishes
-			console.log(data); //this will print on the console the exact object received from the server
-		})
-		.catch(error => {console.log(error);});  //Error handling
+		handleDELETE_TodosAndUser();
 	}
+
 
 	return (
 		<div className="">
