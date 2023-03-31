4c9bb7d7f0b018ff962848852c507055533694f0
Giousned <guille_ranza@hotmail.com>
2023-03-31 12:05:58 +0000

    [ADD] explicacion mentoria

--- src/js/component/ElementList.jsx
+++ src/js/component/ElementList.jsx
@@ -2,9 +2,15 @@ import React from "react";
 
 const ElementList = (props) => {
 
+  const handleElemento = () => {
+
+    props.handleClickSpan(props.elemento)
+
+  }
+
   return (
     <li>
-      <span onClick={() => props.handleClickSpan(props.elemento)}>
+      <span onClick={handleElemento}>
         <i className="fa fa-trash"></i>
       </span>
       {props.elemento.label}
@@ -13,3 +19,14 @@ const ElementList = (props) => {
 };
 
 export default ElementList;
+
+
+{/* TAMBIEN SE PUEDE HACER ASI, DECLARANDO FUNCION ANONIMA Y EJECUTANDOLA EN EL ONCLICK, PERO ES PEOR
+PORQUE LUEGO EL EVENT LISTENER NO PUEDE BORRAR ESA FUNCION CUANDO DEJA DE USARLA
+<li>
+  <span onClick={() => props.handleClickSpan(props.elemento)}>
+    <i className="fa fa-trash"></i>
+  </span>
+  {props.elemento.label}
+</li>
+*/}
